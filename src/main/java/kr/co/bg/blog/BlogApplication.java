615e466d7c47cd9b48800b31714b43b3be5990f5
bg-yu <byunggun.yu@gmail.com>
2023-05-29 23:03:56 +0900

    @EnableJpaAuditing 추가

--- src/main/java/kr/co/bg/blog/BlogApplication.java
+++ src/main/java/kr/co/bg/blog/BlogApplication.java
@@ -2,7 +2,9 @@ package kr.co.bg.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BlogApplication {
 
