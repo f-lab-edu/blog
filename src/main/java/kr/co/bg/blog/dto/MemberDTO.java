1d41e9313fbe372c1a5ad5229845bb5d941fb78a
bg-yu <byunggun.yu@gmail.com>
2023-06-03 02:08:21 +0900

    벨리데이션 태그 추가

--- src/main/java/kr/co/bg/blog/dto/MemberDTO.java
+++ src/main/java/kr/co/bg/blog/dto/MemberDTO.java
@@ -1,16 +1,18 @@
 package kr.co.bg.blog.dto;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class MemberDTO {
-    @NotBlank
+    @Size(min = 1, max = 10)
     private String userId;
 
-    @NotBlank
+    @Pattern(regexp = "[a-zA-Z1-9]{6,12}",
+            message = "비밀번호는 영어와 숫자를 포함한 6~12자리 이내로 입력해주세요")
     private String password;
 
-    @NotBlank
+    @Size(min = 1, max = 10)
     private String name;
 }
