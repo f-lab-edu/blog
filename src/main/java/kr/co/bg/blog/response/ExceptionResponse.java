7d227a5ce68a11dedd8180f464a8513261a441fc
bg-yu <byunggun.yu@gmail.com>
2023-06-03 02:06:54 +0900

    ResponseEntity 상속을 받도록 수정

--- src/main/java/kr/co/bg/blog/response/ExceptionResponse.java
+++ src/main/java/kr/co/bg/blog/response/ExceptionResponse.java
@@ -1,17 +1,30 @@
 package kr.co.bg.blog.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
-public class ExceptionResponse {
+public class ExceptionResponse extends ResponseEntity {
 
-    private Integer status;
+    @Builder
+    public ExceptionResponse(HttpStatus status, String errorCode, Object data) {
+        super(new ExceptionResponseData(status, errorCode, data), status);
+    }
 
-    private String message;
+    @Getter
+    private static class ExceptionResponseData {
+        private Integer status;
 
-    public ExceptionResponse(HttpStatus status, String message) {
-        this.status = status.value();
-        this.message = message;
+        private String errorCode;
+
+        private Object data;
+
+        public ExceptionResponseData(HttpStatus status, String errorCode, Object data) {
+            this.status = status.value();
+            this.errorCode = errorCode;
+            this.data = data;
+        }
     }
 }
