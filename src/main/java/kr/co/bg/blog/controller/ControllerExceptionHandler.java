4fc4eff8c3aaea06adc85f9c917a094af7d50948
bg-yu <byunggun.yu@gmail.com>
2023-06-03 02:06:36 +0900

    벨리데이션 체크 exception 추가

--- src/main/java/kr/co/bg/blog/controller/ControllerExceptionHandler.java
+++ src/main/java/kr/co/bg/blog/controller/ControllerExceptionHandler.java
@@ -1,19 +1,23 @@
 package kr.co.bg.blog.controller;
 
-import kr.co.bg.blog.exception.MemberDuplicatedException;
-import kr.co.bg.blog.exception.MemberException;
 import kr.co.bg.blog.response.ExceptionResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(MemberDuplicatedException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ExceptionResponse memberDuplicatedException(MemberException e) {
-        return new ExceptionResponse(e.getErrorCode().getHttpStatus(), e.getMessage());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity validationCheckException(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        String errorMsg =String.format("%s : %s.", fieldError.getField(), fieldError.getDefaultMessage());
+        return ExceptionResponse.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .data(errorMsg)
+                .build();
     }
 }
