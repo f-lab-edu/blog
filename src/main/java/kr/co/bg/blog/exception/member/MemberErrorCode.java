6d3f1e4cbe40946555ca6c5a9caa84db4a1448bc
bg-yu <byunggun.yu@gmail.com>
2023-06-04 00:33:39 +0900

    final 추가

--- src/main/java/kr/co/bg/blog/exception/member/MemberErrorCode.java
+++ src/main/java/kr/co/bg/blog/exception/member/MemberErrorCode.java
@@ -9,11 +9,11 @@ public enum MemberErrorCode {
     REGIST_MEMBER_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "MEC0001", "MEMBER 등록에 실패 했습니다."),
     DUPLICATED_USER_ID(HttpStatus.CONFLICT, "MEC0002", "이미 등록된 USER_ID 입니다.");
 
-    private HttpStatus httpStatus;
+    private final HttpStatus httpStatus;
 
-    private String errorCode;
+    private final String errorCode;
 
-    private String message;
+    private final String message;
 
     MemberErrorCode(HttpStatus httpStatus, String errorCode, String message) {
         this.httpStatus = httpStatus;
