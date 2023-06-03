f034a9ce8e70f98b7f398eb4d1228d5bd536ea51
bg-yu <byunggun.yu@gmail.com>
2023-06-03 17:56:52 +0900

    테스트 코드를 위해 생성자 및 어노테이션 추가

--- src/main/java/kr/co/bg/blog/dto/MemberDTO.java
+++ src/main/java/kr/co/bg/blog/dto/MemberDTO.java
@@ -1,18 +1,30 @@
 package kr.co.bg.blog.dto;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class MemberDTO {
-    @Size(min = 1, max = 10)
+
+    @NotBlank
+    @Size(min = 2, max = 10)
     private String userId;
 
-    @Pattern(regexp = "[a-zA-Z1-9]{6,12}",
-            message = "비밀번호는 영어와 숫자를 포함한 6~12자리 이내로 입력해주세요")
+    @Pattern(regexp = "[a-zA-Z0-9]{6,12}(?!\\s)",
+            message = "비밀번호는 공백을 제외한 영어, 숫자 조합의 6~12자리만 가능합니다")
     private String password;
 
-    @Size(min = 1, max = 10)
+    @NotBlank
+    @Size(min = 2, max = 10)
     private String name;
+
+    @Builder
+    public MemberDTO(String userId, String password, String name) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+    }
 }
