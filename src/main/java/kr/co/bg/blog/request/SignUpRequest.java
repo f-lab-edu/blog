fed5173845ba733d4eea07a81c67eadabb37be5a
bg-yu <byunggun.yu@gmail.com>
2023-06-11 16:34:50 +0900

    SignUpRequest 추가

---
+++ src/main/java/kr/co/bg/blog/request/SignUpRequest.java
@@ -0,0 +1,24 @@
+package kr.co.bg.blog.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class SignUpRequest {
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private String userId;
+
+    @NotBlank
+    @Pattern(regexp = "[a-zA-Z0-9]{6,12}(?!\\s)",
+            message = "비밀번호는 공백을 제외한 영어, 숫자 조합의 6~12자리만 가능합니다")
+    private String password;
+
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private String name;
+}
